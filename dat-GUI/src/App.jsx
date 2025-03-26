f4983a2c871e9439cbface4ae9b1fdbb379d8e7c
Nikhil Kulkarni <nikhilkulkarni2208@gmail.com>
2025-03-26 13:23:15 +0530

    Refactor GUI with folders

--- dat-GUI/src/App.jsx
+++ dat-GUI/src/App.jsx
@@ -18,19 +18,41 @@ function App() {
     // Initialising GUI 
     const gui = new GUI();
 
-    gui.add(boxMesh.rotation, 'x', 0, 10).name('Rotate X Axis');
-    gui.add(boxMesh.rotation, 'y', 0, Math.PI).name('Rotate Y Axis');
-    gui.add(boxMesh.rotation, 'z', 0, Math.PI).name('Rotate Z Axis');
-    gui.add(boxMesh.scale, 'x', 0, 2).name('Scale X Axis');
-    gui.add(boxMesh.scale, 'y', 0, 2).name('Scale Y Axis');
-    gui.add(boxMesh.scale, 'z', 0, 2).name('Scale Z Axis');
+    // gui.add(boxMesh.rotation, 'x', 0, 10).name('Rotate X Axis');
+    // gui.add(boxMesh.rotation, 'y', 0, Math.PI).name('Rotate Y Axis');
+    // gui.add(boxMesh.rotation, 'z', 0, Math.PI).name('Rotate Z Axis');
+    // gui.add(boxMesh.scale, 'x', 0, 2).name('Scale X Axis');
+    // gui.add(boxMesh.scale, 'y', 0, 2).name('Scale Y Axis');
+    // gui.add(boxMesh.scale, 'z', 0, 2).name('Scale Z Axis');
 
     // Updating Material
+    // const materialParams = {
+    //   boxMeshColor: boxMesh.material.color.getHex()
+    // }
+    // gui.add(boxMesh.material, 'wireframe')
+    // gui.addColor(materialParams, 'boxMeshColor').onChange((value)=> boxMesh.material.color.set(value))
+
+    // Refactor GUI with Folders
+    const geometryFolder = gui.addFolder('Mesh Geometry');
+    geometryFolder.open();
+    const rotationFolder = geometryFolder.addFolder('Rotation');
+    rotationFolder.add(boxMesh.rotation, 'x', 0, Math.PI).name('Rotate X Axis');
+    rotationFolder.add(boxMesh.rotation, 'y', 0, Math.PI).name('Rotate Y Axis');
+    rotationFolder.add(boxMesh.rotation, 'z', 0, Math.PI).name('Rotate Z Axis');
+    const scaleFolder = geometryFolder.addFolder('Scale');
+    scaleFolder.add(boxMesh.scale, 'x', 0, 2).name('Scale X Axis');
+    scaleFolder.add(boxMesh.scale, 'y', 0, 2).name('Scale Y Axis');
+    scaleFolder.add(boxMesh.scale, 'z', 0, 2).name('Scale Z Axis');
+    scaleFolder.open();
+
+    const materialFolder = gui.addFolder('Mesh Material');
     const materialParams = {
-      boxMeshColor: boxMesh.material.color.getHex()
-    }
-    gui.add(boxMesh.material, 'wireframe')
-    gui.addColor(materialParams, 'boxMeshColor').onChange((value)=> boxMesh.material.color.set(value))
+      boxMeshColor: boxMesh.material.color.getHex(),
+    };
+    materialFolder.add(boxMesh.material, 'wireframe');
+    materialFolder
+      .addColor(materialParams, 'boxMeshColor')
+      .onChange((value) => boxMesh.material.color.set(value));
   }, []);
 
   return (
