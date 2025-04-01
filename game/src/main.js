303350c35ed5ffb43adbc7684dd9561c2a68a5de
Nikhil Kulkarni <nikhilkulkarni2208@gmail.com>
2025-04-01 13:33:31 +0530

    cube and ground

--- game/src/main.js
+++ game/src/main.js
@@ -1,31 +1,43 @@
-import * as THREE from 'three'
-import { OrbitControls } from 'three/addons/controls/OrbitControls.js'
+import * as THREE from "three";
+import { OrbitControls } from "three/addons/controls/OrbitControls.js";
 
-  const scene = new THREE.Scene()
-  const camera = new THREE.PerspectiveCamera(
-    75,
-    window.innerWidth / window.innerHeight,
-    0.1,
-    1000
-  )
+const scene = new THREE.Scene();
+const camera = new THREE.PerspectiveCamera(
+  75,
+  window.innerWidth / window.innerHeight,
+  0.1,
+  1000
+);
 
-  const renderer = new THREE.WebGLRenderer()
-  renderer.setSize(window.innerWidth, window.innerHeight)
-  document.body.appendChild(renderer.domElement)
+const renderer = new THREE.WebGLRenderer();
+renderer.setSize(window.innerWidth, window.innerHeight);
+document.body.appendChild(renderer.domElement);
 
-  const controls = new OrbitControls(camera, renderer.domElement)
+const controls = new OrbitControls(camera, renderer.domElement);
 
-  const geometry = new THREE.BoxGeometry(1, 1, 1)
-  const material = new THREE.MeshBasicMaterial({ color: 0x00ff00 })
-  const cube = new THREE.Mesh(geometry, material)
-  scene.add(cube)
+const light = new THREE.DirectionalLight(0xffffff, 1);
+light.position.y = 3;
+light.position.z = 2;
+scene.add(light);
 
-  camera.position.z = 5
+const geometry = new THREE.BoxGeometry(1, 1, 1);
+const material = new THREE.MeshStandardMaterial({ color: 0x00ff00 });
+const cube = new THREE.Mesh(geometry, material);
+scene.add(cube);
 
-  function animate() {
-    requestAnimationFrame(animate)
-    renderer.render(scene, camera)
-    cube.rotation.x += 0.01
-    cube.rotation.y += 0.01
-  }
-  animate()
+const ground = new THREE.Mesh(
+  new THREE.BoxGeometry(5, 0.1, 10),
+  new THREE.MeshStandardMaterial({ color: 0x0000ff })
+);
+ground.position.y = -1
+scene.add(ground);
+
+camera.position.z = 5;
+
+function animate() {
+  requestAnimationFrame(animate);
+  renderer.render(scene, camera);
+  cube.rotation.x += 0.01;
+  cube.rotation.y += 0.01;
+}
+animate();
